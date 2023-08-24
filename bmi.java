0fe89539b61d09a444d97a1a582a02225065860b
arjunapriyo <arjuna_priyo_32rpl@student_smktelkom-mlg.sch.id>
2023-08-24 13:47:33 +0700

    TugasSekolah

---
+++ bmi.java
@@ -0,0 +1,45 @@
+
+
+
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MOKLET010
+ */
+public class bmi {
+    public static void main(String[] args) {
+        String nama = JOptionPane.showInputDialog("Masukkan nama (nama panggilan saja) : ");
+        String beratAsli = JOptionPane.showInputDialog("Masukkan berat badan(kg) : ");
+        String tinggiAsli = JOptionPane.showInputDialog("Masukkan tinggi badan(cm) : ");
+        
+        DecimalFormat numberFormat = new DecimalFormat("#.0");
+        
+        float beratPound = Float.parseFloat(beratAsli)*2.20462f;
+        float tinggiInci = Float.parseFloat(tinggiAsli)/2.54f;
+        
+        float hasil = beratPound/(tinggiInci*tinggiInci)*703f;
+        
+        JOptionPane.showMessageDialog(null, "berat badan (pound) = "+numberFormat.format(beratPound)+"\ntinggi badan (inch) ="+numberFormat.format(tinggiInci));
+        JOptionPane.showMessageDialog(null, "BMI ananda "+nama+" adalah "+numberFormat.format(hasil));
+        
+        if (hasil < 18.5) {
+            JOptionPane.showMessageDialog(null, "anda kekurangan berat badan!");
+        }
+        else if (hasil >= 18.5 & hasil < 25) {
+            JOptionPane.showMessageDialog(null, "berat badan anda normal!");
+        }
+        else if (hasil >= 25 & hasil < 30) {
+            JOptionPane.showMessageDialog(null, "anda kelebihan berat badan!");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "anda obesitas!");
+        }
+    }
+}
